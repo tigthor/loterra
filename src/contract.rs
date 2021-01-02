e7a9c349fd9ff2694c4ac987bda9d291387ce9c3
0xantman <antho74830@gmail.com>
2021-01-02 19:34:04 +0100

    Work in progress...

--- src/contract.rs
+++ src/contract.rs
@@ -1,34 +1,36 @@
-use cosmwasm_std::{to_binary, Context, Api, Binary, Env, Extern, HandleResponse, InitResponse, MessageInfo, Querier, StdResult, Storage, BankMsg, Coin, StakingMsg, StakingQuery, StdError, AllDelegationsResponse, HumanAddr};
+use cosmwasm_std::{to_binary, Context, Api, Binary, Env, Deps, DepsMut, HandleResponse, InitResponse, MessageInfo, Querier, StdResult, Storage, BankMsg, Coin, StakingMsg, StakingQuery, StdError, AllDelegationsResponse, HumanAddr, Uint128, Delegation};
 
 use crate::error::ContractError;
-use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use crate::msg::{HandleMsg, InitMsg, QueryMsg, ConfigResponse};
 use crate::state::{config, config_read, State};
 use cosmwasm_std::testing::StakingQuerier;
 use crate::error::ContractError::Std;
 use std::fs::canonicalize;
 use rand::Rng;
 
+
 // Note, you can use StdResult in some functions where you do not
 // make use of the custom errors
-pub fn init<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+pub fn init(
+    deps: DepsMut,
     _env: Env,
     info: MessageInfo,
     msg: InitMsg,
 ) -> StdResult<InitResponse> {
     let state = State {
-        owner: deps.api.canonical_address(&_env.contract.address)?,
+        owner: deps.api.canonical_address(&info.sender)?,
         players: vec![],
         everyBlockHeight: 0,
-        denom: "ulot".to_string()
+        denom: "ujack".to_string()
     };
-    config(&mut deps.storage).save(&state)?;
+    config(deps.storage).save(&state)?;
     Ok(InitResponse::default())
 }
 
+
 // And declare a custom Error variant for the ones where you will want to make use of it
-pub fn handle<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+pub fn handle(
+    deps: DepsMut,
     _env: Env,
     info: MessageInfo,
     msg: HandleMsg,
@@ -39,33 +41,42 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     }
 }
 
-pub fn handle_register<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+pub fn handle_register(
+    deps: DepsMut,
     _env: Env,
     info: MessageInfo
 ) -> Result<HandleResponse, ContractError> {
-    // Ensure message sender is the owner
-    let mut state = config(&mut Storage).load()?;
-    if _env.contract.address != state.owner {
-        return Err(ContractError::Unauthorized {});
+
+    let mut state = config(deps.storage).load()?;
+
+    // Ensure sender are not sending wrong coins
+    for coin in info.sent_funds {
+        if coin.denom != state.denom{
+            return Err(ContractError::Unauthorized {});
+            // return Err(ContractError::Std(StdError::GenericErr { msg: format!("send only {} to buy tickets lottery", state.denom), backtrace: None }));
+        }
     }
+
     // Ensure message sender is delegating some funds to the lottery validator
     //let delegations = deps.querier.query_delegation(delegator: info.sender, validator: _env.contract.address).clone()?;
-    let delegations = StakingQuery::Delegation { delegator: info.sender, validator: _env.contract.address };
-
-    deps.querier.query_delegation();
-    if delegations.amount <= 0 {
-        return Err(ContractError::Std(StdError::GenericErr { msg: "you need to delegate some funds to the validator first".to_string(), backtrace: None }));
+    //let delegation = deps.querier.query_delegation(info.sender, deps.api.human_address(&state.owner)?)??;
+    //let delegation = StakingQuery::Delegation{ delegator: info.sender.into(), validator: _env.contract.address.into() }.into();
+/*
+    if delegation.amount.amount <= 0 {
+        return Err(ContractError::Unauthorized {});
+        //return Err(ContractError::Std(StdError::GenericErr { msg: "you need to delegate some funds to the validator first".to_string(), backtrace: () }));
     }
     // Ensure message sender is sending some funds to buy the lottery
     if info.sent_funds.is_empty() {
-        return Err(ContractError::Std(StdError::GenericErr { msg: format!("send some {} to buy tickets lottery", state.denom), backtrace: None }));
+        return Err(ContractError::Unauthorized {});
+        //return Err(ContractError::Std(StdError::GenericErr { msg: format!("send some {} to buy tickets lottery", state.denom), backtrace: Backtrace }));
     }
     // Ensure message sender is sending the rights denom tickets and add register
     for coin in info.sent_funds {
-        if coin.denom != state.denom{
-            return Err(ContractError::Std(StdError::GenericErr { msg: format!("send only {} to buy tickets lottery", state.denom), backtrace: None }));
-        }
+        /*if coin.denom != state.denom{
+            return Err(ContractError::Unauthorized {});
+            // return Err(ContractError::Std(StdError::GenericErr { msg: format!("send only {} to buy tickets lottery", state.denom), backtrace: None }));
+        }*/
         /*
             TODO: Ensure the tickets are Integers and not Decimals
         */
@@ -74,32 +85,53 @@ pub fn handle_register<S: Storage, A: Api, Q: Querier>(
         }
     }
     // Save the state
-    config(&mut Storage).save(&state);
+    config(deps.storage).save(&state);
+
+    */
 
     Ok(HandleResponse::default())
 }
 
-pub fn handle_play<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+pub fn handle_play(
+    deps: DepsMut,
     _env: Env,
     info: MessageInfo
 ) -> Result<HandleResponse, ContractError> {
+    /*
     let mut state = config(&mut Storage).load()?;
     // Ensure message sender is the owner of the contract
-    if  info.sender != _env.contract.address {
+    if  info.sender != state.owner {
         return Err(ContractError::Unauthorized {});
     }
     // Play the lottery
     let players = state.players;
-    let mut range = rand::thread_rng();
+    let mut rng = rand::thread_rng();
     let maxRange = players.len() - 1;
-    range.gen_range(0..maxRange);
+    let range = rng.gen_range(0..maxRange);
+
+    let winner = players[range].clone();
+    let winnerDelegation = deps.querier.query_delegation(deps.api.human_address( &winner)?, deps.api.human_address(&state.owner)?)??;
+
     /*
         TODO: Compare the winner weight to others players
      */
+    // Ensure there is no duplicate address in the players vector
+    let mut dedupPlayers = players.clone();
+    dedupPlayers.sort_unstable();
+    dedupPlayers.dedup();
+
+    let mut totalDelegation: Uint128 = Uint128(0);
+    for player in dedupPlayers {
+        let delegation = deps.querier.query_delegation(deps.api.human_address( &player)?, deps.api.human_address(&state.owner)?)??;
+        totalDelegation += delegation.amount.amount;
+    }
+    // Jackpot winner weight due
+    //let jackpotWeight = winnerDelegation.amount.amount * Uint128(100) / totalDelegation;
     /*
         TODO: Calcule the amount to send to the winner
      */
+
+
     /*
         TODO: Withdraw rewards and commissions
      */
@@ -111,12 +143,12 @@ pub fn handle_play<S: Storage, A: Api, Q: Querier>(
     /*
         TODO: Send a transaction to the winner
      */
-
+*/
     Ok(HandleResponse::default())
 }
 
-pub fn query<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
+pub fn query(
+    deps: Deps,
     _env: Env,
     msg: QueryMsg,
 ) -> StdResult<Binary> {
@@ -125,8 +157,8 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
     }
 }
 
-fn query_config<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<ConfigResponse> {
-    let state = config_read(&deps.storage).load()?;
+fn query_config(deps: Deps) -> StdResult<ConfigResponse> {
+    let state = config_read(deps.storage).load()?;
     Ok(state)
 }
 
@@ -134,16 +166,41 @@ fn query_config<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdRe
 mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info, BankQuerier, MOCK_CONTRACT_ADDR, MockStorage};
-    use cosmwasm_std::{coins, from_binary, Validator, Decimal, FullDelegation, HumanAddr, Uint128, MessageInfo, StdError, Storage};
+    use cosmwasm_std::{coins, from_binary, Validator, Decimal, FullDelegation, HumanAddr, Uint128, MessageInfo, StdError, Storage, Api, CanonicalAddr};
     use std::collections::HashMap;
     use std::borrow::Borrow;
     use cosmwasm_storage::{bucket_read, bucket, singleton, singleton_read, };
     use schemars::JsonSchema;
     use serde::{Deserialize, Serialize};
-    use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+    use crate::msg::{HandleMsg, InitMsg, QueryMsg};
+
+
+
     #[test]
-    fn test_participate_lottery() {
-        let mut deps = mock_dependencies(&[]);
+    fn proper_init (){
+        let mut deps = mock_dependencies(&[Coin{ denom: "uscrt".to_string(), amount: Uint128(100_000_000)}]);
+
+        let info = mock_info(HumanAddr::from("owner"), &[]);
+        let init_msg = InitMsg {
+            denom: "ujack".to_string()
+        };
+
+        let res = init(deps.as_mut(), mock_env(), info, init_msg).unwrap();
+        assert_eq!(0, res.messages.len());
+
+        let res = query_config(deps.as_ref()).unwrap();
+
+        assert_eq!( "00000000006E00000000007772000000006F650000000000", CanonicalAddr::to_string(&res.owner));
+
+    }
+    /*#[test]
+    fn register() {
+        let mut deps = mock_dependencies(&[Coin{ denom: "uscrt".to_string(), amount: Uint128(100_000_000)}]);
+
+        let msg = InitMsg {
+            denom: "ujack".to_string()
+        };
+
         let env = mock_env();
         let info = mock_info("rico", &[Coin{ denom: "ux".to_string(), amount: Uint128(2)}]);
         //Init the data
@@ -189,7 +246,8 @@ mod tests {
         let state2 = reader2.load();
         println!("{:?}", state2);*/
     }
-    #[test]
+ */
+/*    #[test]
     fn run_lottery(){
         let environment= mock_env();
         let info = MessageInfo{ sender: HumanAddr::from(MOCK_CONTRACT_ADDR), sent_funds: vec![]};
@@ -231,7 +289,7 @@ mod tests {
         println!("{:?}", stakingWithdraw);
 
         println!("{:?}", environment)
-    }
+    }*/
     /*#[test]
     fn proper_initialization() {
         let mut deps = mock_dependencies(&[]);
