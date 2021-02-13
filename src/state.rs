263fc3af30816803e0991cbac0eb96b50a4dfd22
0xantman <antho74830@gmail.com>
2021-02-13 18:55:54 +0100

    Migration security mesure in progress...

--- src/state.rs
+++ src/state.rs
@@ -93,6 +93,7 @@ pub enum Proposal {
     JackpotRewardPercentage,
     ClaimEveryBlock,
     AmountToRegister,
+    SecurityMigration,
     // test purpose
     NotExist,
 }
@@ -109,6 +110,7 @@ pub struct PollInfoState {
     pub amount: Uint128,
     pub prize_rank: Vec<u8>,
     pub proposal: Proposal,
+    pub migration_address: Option<HumanAddr>
 }
 
 pub fn poll_storage<T: Storage>(storage: &mut T) -> Bucket<T, PollInfoState> {
