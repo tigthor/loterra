29e4ca7c9d38ce1191939da4d8ff9987cd941847
tigthor <b.thierry1337@gmail.com>
2021-05-10 11:34:11 +0200

    Code refactoring

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,10 @@
+use crate::msg::QueryMsg;
+use crate::query::{GetHolderResponse, LoterraBalanceResponse, TerrandResponse};
+use crate::state::State;
+use cosmwasm_std::{
+    to_binary, Api, CanonicalAddr, Coin, CosmosMsg, Empty, Extern, HumanAddr, Querier,
+    QueryRequest, StdResult, Storage, Uint128, WasmMsg, WasmQuery,
+};
 pub fn count_match(x: &str, y: &str) -> usize {
     let mut count = 0;
     for i in 0..y.len() {
@@ -6,4 +13,88 @@ pub fn count_match(x: &str, y: &str) -> usize {
         }
     }
     count
+}
+// There is probably some built-in function for this, but this is a simple way to do it
+pub fn is_lower_hex(combination: &str, len: u8) -> bool {
+    if combination.len() != (len as usize) {
+        return false;
+    }
+    if !combination
+        .chars()
+        .all(|c| ('a'..='f').contains(&c) || ('0'..='9').contains(&c))
+    {
+        return false;
+    }
+    true
+}
+
+pub fn encode_msg_execute(
+    msg: QueryMsg,
+    address: HumanAddr,
+    coin: Vec<Coin>,
+) -> StdResult<CosmosMsg> {
+    Ok(WasmMsg::Execute {
+        contract_addr: address,
+        msg: to_binary(&msg)?,
+        send: coin,
+    }
+    .into())
+}
+pub fn encode_msg_query(msg: QueryMsg, address: HumanAddr) -> StdResult<QueryRequest<Empty>> {
+    Ok(WasmQuery::Smart {
+        contract_addr: address,
+        msg: to_binary(&msg)?,
+    }
+    .into())
+}
+pub fn wrapper_msg_terrand<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    query: QueryRequest<Empty>,
+) -> StdResult<TerrandResponse> {
+    let res: TerrandResponse = deps.querier.query(&query)?;
+    Ok(res)
+}
+
+pub fn wrapper_msg_loterra<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    query: QueryRequest<Empty>,
+) -> StdResult<LoterraBalanceResponse> {
+    let res: LoterraBalanceResponse = deps.querier.query(&query)?;
+
+    Ok(res)
+}
+
+pub fn wrapper_msg_loterra_staking<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    query: QueryRequest<Empty>,
+) -> StdResult<GetHolderResponse> {
+    let res: GetHolderResponse = deps.querier.query(&query)?;
+
+    Ok(res)
+}
+
+pub fn user_total_weight<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    state: &State,
+    address: &CanonicalAddr,
+) -> Uint128 {
+    let mut weight = Uint128::zero();
+    let human_address = deps.api.human_address(&address).unwrap();
+
+    // Ensure sender have some reward tokens
+    let msg = QueryMsg::Holder {
+        address: human_address,
+    };
+    let loterra_human = deps
+        .api
+        .human_address(&state.loterra_staking_contract_address.clone())
+        .unwrap();
+    let res = encode_msg_query(msg, loterra_human).unwrap();
+    let loterra_balance = wrapper_msg_loterra_staking(&deps, res).unwrap();
+
+    if !loterra_balance.balance.is_zero() {
+        weight += loterra_balance.balance;
+    }
+
+    weight
 }
